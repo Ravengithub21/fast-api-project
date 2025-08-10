d59601e58799001c05d9081dd23adf94663744c0
Milosz <milosz.domagalaki@gmail.com>
2025-08-10 20:53:21 +0200

    created a function that returns the whole todo list

--- project.py
+++ project.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI()                                             
+
+
+all_todos = [
+        {"todo_id" : 1, "todo_name" : "Sports", "todo_description" : "Go to the gym"},
+        {"todo_id" : 2, "todo_name" : "Gaming", "todo_description" : "Playing souls"},
+        {"todo_id" : 3, "todo_name" : "Shopping", "todo_description" : "Buy groceries"},
+        {"todo_id" : 4, "todo_name" : "Meditate", "todo_description" : "Meditate for 20min"},
+        {"todo_id" : 5, "todo_name" : "Running", "todo_description" : "Go for a 1hour run!"}
+]
+
 
 @app.get("/")
 def read_root():
     return {"Hello" : "World"}
+
+
+
+
+
+@app.get("/todos")
+def get_todo():
+    return all_todos
